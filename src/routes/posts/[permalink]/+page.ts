74cdd3720ee77293df73e3575d848f39490b1560
Josh Nussbaum <joshnuss@gmail.com>
2025-07-25 11:55:17 -0400

    Remove unused codes

--- src/routes/posts/[permalink]/+page.ts
+++ src/routes/posts/[permalink]/+page.ts
@@ -1,8 +1,6 @@
 import { error, type ServerLoad } from '@sveltejs/kit'
-import * as p from '$posts/foo.md'
 
 export const load: ServerLoad = async ({ params }) => {
-  console.log(p)
   try {
     const post = await import(`$posts/${params.permalink}.md`)
 
