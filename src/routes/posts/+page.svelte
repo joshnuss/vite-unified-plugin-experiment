dc1447030a677eb079ea26ba39a06a6c564ad7ec
Josh Nussbaum <joshnuss@gmail.com>
2025-07-25 20:43:27 -0400

    Adds some ui

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -3,4 +3,11 @@
   let { posts } = $derived(data)
 </script>
 
+{#each posts as post}
+  <article>
+    <h2>{post.title}</h2>
+    <a href="/posts/{post.id}">View post</a>
+  </article>
+{/each}
+
 <pre>{JSON.stringify(posts, null, 2)}</pre>
--- src/routes/posts/[permalink]/+page.svelte
+++ src/routes/posts/[permalink]/+page.svelte
@@ -3,4 +3,7 @@
   let { post } = $derived(data)
 </script>
 
+<h1>{post.title}</h1>
+{@html post.body}
+
 <pre>{JSON.stringify(post, null, 2)}</pre>
